d5165bdc17b8175fce734f1e9134ef110e6f468e
tmtocb <kittz.esport@gmail.com>
2021-03-05 13:11:00 +0100

    add Semantic UI CSS & jQuery via yarn

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,4 +2,6 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("stylesheets/application.scss") 
+require("stylesheets/application.scss")
+import 'semantic-ui-css/semantic.min.css'; 
+require("semantic-ui-css")
