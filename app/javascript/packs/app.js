ea8f662206fef3b85c505b4fc7deec62abe5fc99
tmtocb <kittz.esport@gmail.com>
2021-03-05 14:39:20 +0100

    link Rails API with Vue front via axios

--- app/javascript/packs/app.js
+++ app/javascript/packs/app.js
@@ -1,15 +1,11 @@
 import Vue from 'vue';
+const Api = require('./api');
 
 document.addEventListener("DOMContentLoaded", () => {
   var app = new Vue({
     el: '#app',
     data: {
-      tasks: [
-        { id: 1, name: 'Todo 1', description: 'This is a todo', completed: false },
-        { id: 2, name: 'Todo 2', description: 'This is a todo', completed: false },
-        { id: 3, name: 'Todo 411', description: 'This is a todo', completed: true },
-        { id: 4, name: 'Todo 4', description: 'This is a todo', completed: true }
-      ],
+      tasks: [],
       task: {},
       message: '',
       action: 'create'
