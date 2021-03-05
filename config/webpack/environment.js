b42e27240dc96cfa3fc084d5295d0c6a58e67d5a
tmtocb <kittz.esport@gmail.com>
2021-03-05 13:39:28 +0100

    add Vue.js via yarn, create Vue app

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,7 @@
 const { environment } = require('@rails/webpacker')
 const webpack = require("webpack")
+const customConfig = require('./custom')
+environment.config.merge(customConfig)
 
 environment.plugins.append("Provide", new webpack.ProvidePlugin({
   $: 'jquery',
